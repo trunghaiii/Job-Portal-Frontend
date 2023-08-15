5e12db20e531e34a8ea7f522b1a9885720c47b34
trunghaiii <haitran022001@gmail.com>
2023-08-14 23:31:09 -0400

    set up basic redux toolkit

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,9 +1,28 @@
 
+import { useSelector, useDispatch } from 'react-redux'
+import { decrement, increment } from '../../redux/slices/counterSlice'
 
 const Home = () => {
+
+    const count = useSelector((state: any) => state.counter.value)
+    const dispatch = useDispatch()
     return (
         <div>
-            Homeeeee
+            <div>
+                <button
+                    aria-label="Increment value"
+                    onClick={() => dispatch(increment())}
+                >
+                    Increment
+                </button>
+                <span>{count}</span>
+                <button
+                    aria-label="Decrement value"
+                    onClick={() => dispatch(decrement())}
+                >
+                    Decrement
+                </button>
+            </div>
         </div>
     )
 }
