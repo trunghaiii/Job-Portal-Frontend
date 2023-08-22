4c61a926e45045d086802de0c5d045b3b03279e5
trunghaiii <haitran022001@gmail.com>
2023-08-21 20:07:55 -0400

    complete designing Login page wit antd and scss

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,14 +1,46 @@
 
 import './Login.scss';
-import { Card } from 'antd';
+import { Card, Button, Form, Input } from 'antd';
 
 const Login = () => {
+
+    const onFinish = (values: any) => {
+        console.log('Success:', values);
+    };
+
     return (
         <div className='login-container'>
-            <Card bordered={false} style={{ width: 400, height: "60%" }}>
-                <p>Card content</p>
-                <p>Card content</p>
-                <p>Card content</p>
+            <Card className='login-card' bordered={false}>
+                <h1>Login</h1>
+                <Form
+                    name="basic"
+                    labelCol={{ span: 24 }}
+                    wrapperCol={{ span: 24 }}
+                    onFinish={onFinish}
+                    autoComplete="off"
+                >
+                    <Form.Item
+                        label="Username"
+                        name="username"
+                        rules={[{ required: true, message: 'Please input your username!' }]}
+                    >
+                        <Input />
+                    </Form.Item>
+
+                    <Form.Item
+                        label="Password"
+                        name="password"
+                        rules={[{ required: true, message: 'Please input your password!' }]}
+                    >
+                        <Input.Password />
+                    </Form.Item>
+
+                    <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+                        <Button type="primary" htmlType="submit">
+                            Login
+                        </Button>
+                    </Form.Item>
+                </Form>
             </Card>
         </div>
     )
