a500f35377d3787c219cc0059b3b4e4356726d08
trunghaiii <haitran022001@gmail.com>
2023-09-07 16:45:15 -0400

    design basic frame for admin page

--- src/components/AdminLayout/AdminLayout.tsx
+++ src/components/AdminLayout/AdminLayout.tsx
@@ -1,13 +1,89 @@
-import { Outlet } from "react-router-dom"
+import React, { useState } from 'react';
+import {
+    MenuFoldOutlined,
+    MenuUnfoldOutlined,
+    UploadOutlined,
+    UserOutlined,
+    VideoCameraOutlined,
+} from '@ant-design/icons';
+
+import { MdOutlineDashboard } from 'react-icons/md';
+import { HiOutlineBuildingOffice2 } from 'react-icons/hi2';
+
+import { Layout, Menu, Button, theme } from 'antd';
+
+const { Header, Sider, Content } = Layout;
+
+import { Outlet, useNavigate } from "react-router-dom"
 
 
 const AdminLayout = () => {
+
+    const [collapsed, setCollapsed] = useState(false);
+    const navigate = useNavigate()
+    const {
+        token: { colorBgContainer },
+    } = theme.useToken();
+
     return (
-        <div>
-            <div>admin header</div>
-            <div><Outlet /></div>
-            <div>admin footer</div>
-        </div>
+        <Layout style={{ height: "100%" }}>
+            <Sider trigger={null} collapsible collapsed={collapsed}>
+                <div className="demo-logo-vertical" />
+                <Menu
+                    theme="dark"
+                    mode="inline"
+                    defaultSelectedKeys={['1']}
+                    items={[
+                        {
+                            key: '1',
+                            // icon: <UserOutlined />,
+                            label: 'ADMIN PAGE',
+                        },
+                        {
+                            key: '2',
+                            icon: <MdOutlineDashboard />,
+                            label: 'Dash Board',
+                        },
+                        {
+                            key: '3',
+                            icon: <UserOutlined />,
+                            label: 'Users',
+                        },
+                        {
+                            key: '4',
+                            icon: <div onClick={() => navigate("/admin/company")}><HiOutlineBuildingOffice2 /></div>,
+                            label: <div onClick={() => navigate("/admin/company")}>Companies</div>,
+                        },
+                    ]}
+                />
+            </Sider>
+            <Layout>
+                <Header style={{ padding: 0, background: colorBgContainer }}>
+                    <Button
+                        type="text"
+                        icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+                        onClick={() => setCollapsed(!collapsed)}
+                        style={{
+                            fontSize: '16px',
+                            width: 64,
+                            height: 64,
+                        }}
+                    />
+                </Header>
+                <Content
+                    style={{
+                        margin: '24px 16px',
+                        padding: 24,
+                        minHeight: 280,
+                        background: colorBgContainer,
+                    }}
+                >
+                    <div><Outlet /></div>
+                </Content>
+            </Layout>
+        </Layout>
+
+
     )
 }
 
