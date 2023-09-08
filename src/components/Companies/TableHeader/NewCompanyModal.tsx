9edfd2a5614b8fca32abf390de030ecb76da6dd7
trunghaiii <haitran022001@gmail.com>
2023-09-08 12:37:24 -0400

    design NewCompany Modal

--- src/components/Companies/TableHeader/NewCompanyModal.tsx
+++ src/components/Companies/TableHeader/NewCompanyModal.tsx
@@ -1,6 +1,6 @@
 
 import React, { useState } from 'react';
-import { Button, Modal } from 'antd';
+import { Button, Modal, Checkbox, Form, Input } from 'antd';
 
 
 interface IProps {
@@ -8,12 +8,27 @@ interface IProps {
     setOpenNewCompanyModal: any
 }
 
+type FieldType = {
+    companyName?: string;
+    address?: string;
+    description?: string;
+};
+
 const NewCompanyModal = (props: IProps) => {
+
+    const [form] = Form.useForm();
+
+    const { TextArea } = Input;
 
     const { openNewCompanyModal, setOpenNewCompanyModal } = props
 
-    const handleOk = () => {
-        setOpenNewCompanyModal(false);
+    const onFinish = (values: any) => {
+        console.log('Success:', values);
+    };
+
+    const handleCreateCompany = () => {
+        form.submit()
+        //setOpenNewCompanyModal(false);
     };
 
     const handleCancel = () => {
@@ -25,11 +40,48 @@ const NewCompanyModal = (props: IProps) => {
             width={"50%"}
             title="Create New Company"
             open={openNewCompanyModal}
-            onOk={handleOk}
-            onCancel={handleCancel}>
-            <p>Some contents...</p>
-            <p>Some contents...</p>
-            <p>Some contents...</p>
+            onCancel={handleCancel}
+            footer={[
+                <Button key="back" onClick={handleCancel}>
+                    Cancel
+                </Button>,
+                <Button key="submit" type="primary" onClick={handleCreateCompany}>
+                    Create
+                </Button>
+            ]}
+        >
+
+            <Form
+                form={form}
+                name="basic"
+                labelCol={{ span: 24 }}
+                wrapperCol={{ span: 24 }}
+                onFinish={onFinish}
+                autoComplete="off"
+            >
+                <Form.Item<FieldType>
+                    label="Company Name"
+                    name="companyName"
+                    rules={[{ required: true, message: 'Please input Company Name!' }]}
+                >
+                    <Input />
+                </Form.Item>
+                <Form.Item<FieldType>
+                    label="Address"
+                    name="address"
+                    rules={[{ required: true, message: 'Please input Company Address!' }]}
+                >
+                    <TextArea />
+                </Form.Item>
+                <Form.Item<FieldType>
+                    label="Description"
+                    name="description"
+                    rules={[{ required: true, message: 'Please input Company Description!' }]}
+                >
+                    <TextArea />
+                </Form.Item>
+
+            </Form>
         </Modal>
     )
 }
