2b7b61f78eb126aeba71710d0947fad58239b354
trunghaiii <haitran022001@gmail.com>
2023-09-26 20:46:51 -0400

    design base home page

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,13 +1,61 @@
 
+import { Card } from 'antd';
 import { decrement, increment } from '../../redux/slices/counterSlice'
+import "./Home.scss"
 
 const Home = () => {
 
 
+    const { Meta } = Card;
+
     return (
-        <div>
-            <div>
-                Homeeeeee
+        <div className='home-container'>
+            <h3 className='home-title'>Companies</h3>
+            <div className='company-card'>
+                <Card
+                    hoverable
+                    style={{ width: 300, height: 350 }}
+                    cover={<img
+                        alt="example"
+                        src="http://localhost:9000/images/companylogos/Plexxis-Logo-1695747169796.png"
+                        height={280}
+                    />}
+                >
+                    <Meta style={{ textAlign: "center" }} title="Plexxis Software" />
+                </Card>
+                <Card
+                    hoverable
+                    style={{ width: 300, height: 350 }}
+                    cover={<img
+                        alt="example"
+                        src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1200px-Google_2015_logo.svg.png"
+                        height={280}
+                    />}
+                >
+                    <Meta style={{ textAlign: "center" }} title="Google" />
+                </Card>
+                <Card
+                    hoverable
+                    style={{ width: 300, height: 350 }}
+                    cover={<img
+                        alt="example"
+                        src="http://localhost:9000/images/companylogos/Plexxis-Logo-1695747169796.png"
+                        height={280}
+                    />}
+                >
+                    <Meta style={{ textAlign: "center" }} title="Plexxis Software" />
+                </Card>
+                <Card
+                    hoverable
+                    style={{ width: 300, height: 350 }}
+                    cover={<img
+                        alt="example"
+                        src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1200px-Google_2015_logo.svg.png"
+                        height={280}
+                    />}
+                >
+                    <Meta style={{ textAlign: "center" }} title="Google" />
+                </Card>
             </div>
         </div>
     )
