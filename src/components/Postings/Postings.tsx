f5186957c735bdb05d0f71482eaf4379ecb7d9db
trunghaiii <haitran022001@gmail.com>
2023-09-28 17:33:20 -0400

    responsively design Postings component

--- src/components/Postings/Postings.tsx
+++ src/components/Postings/Postings.tsx
@@ -1,4 +1,4 @@
-import { Card } from "antd";
+import { Card, Pagination } from "antd";
 import { CiLocationOn } from 'react-icons/ci';
 import "./Postings.scss"
 
@@ -6,60 +6,65 @@ import "./Postings.scss"
 const Postings = () => {
     return (
         <div className="posting-container">
-            <Card style={{ width: 400, height: 250, cursor: "pointer", border: "1px solid" }}>
-                <div className="posting-title">
-                    <img
-                        src="http://localhost:9000/images/companylogos/Plexxis-Logo-1695747169796.png"
-                        width={60}
-                        height={60}
-                        alt="" />
-                    <h2>Software Engineer</h2>
-                </div>
-                <div className="posting-detail">
-                    <div className="location">
-                        <p><CiLocationOn /></p>
-                        <p>Ontario</p>
-                        <p style={{ margin: 0, marginLeft: "10px" }}>100,000 cad</p>
+            <div className="posting-card">
+                <Card style={{ width: 400, height: 250, cursor: "pointer", border: "1px solid" }}>
+                    <div className="posting-title">
+                        <img
+                            src="http://localhost:9000/images/companylogos/Plexxis-Logo-1695747169796.png"
+                            width={60}
+                            height={60}
+                            alt="" />
+                        <h2>Software Engineer</h2>
                     </div>
-                    <p >Posting Time: 1/2/1999</p>
-                </div>
-            </Card>
-            <Card style={{ width: 400, height: 250, cursor: "pointer", border: "1px solid" }}>
-                <div className="posting-title">
-                    <img
-                        src="http://localhost:9000/images/companylogos/Plexxis-Logo-1695747169796.png"
-                        width={60}
-                        height={60}
-                        alt="" />
-                    <h2>Software Engineer</h2>
-                </div>
-                <div className="posting-detail">
-                    <div className="location">
-                        <p><CiLocationOn /></p>
-                        <p>Ontario</p>
-                        <p style={{ margin: 0, marginLeft: "10px" }}>100,000 cad</p>
+                    <div className="posting-detail">
+                        <div className="location">
+                            <p><CiLocationOn /></p>
+                            <p>Ontario</p>
+                            <p style={{ margin: 0, marginLeft: "10px" }}>100,000 cad</p>
+                        </div>
+                        <p >Posting Time: 1/2/1999</p>
                     </div>
-                    <p >Posting Time: 1/2/1999</p>
-                </div>
-            </Card>
-            <Card style={{ width: 400, height: 250, cursor: "pointer", border: "1px solid" }}>
-                <div className="posting-title">
-                    <img
-                        src="http://localhost:9000/images/companylogos/Plexxis-Logo-1695747169796.png"
-                        width={60}
-                        height={60}
-                        alt="" />
-                    <h2>Software Engineer</h2>
-                </div>
-                <div className="posting-detail">
-                    <div className="location">
-                        <p><CiLocationOn /></p>
-                        <p>Ontario</p>
-                        <p style={{ margin: 0, marginLeft: "10px" }}>100,000 cad</p>
+                </Card>
+                <Card style={{ width: 400, height: 250, cursor: "pointer", border: "1px solid" }}>
+                    <div className="posting-title">
+                        <img
+                            src="http://localhost:9000/images/companylogos/Plexxis-Logo-1695747169796.png"
+                            width={60}
+                            height={60}
+                            alt="" />
+                        <h2>Software Engineer</h2>
                     </div>
-                    <p >Posting Time: 1/2/1999</p>
-                </div>
-            </Card>
+                    <div className="posting-detail">
+                        <div className="location">
+                            <p><CiLocationOn /></p>
+                            <p>Ontario</p>
+                            <p style={{ margin: 0, marginLeft: "10px" }}>100,000 cad</p>
+                        </div>
+                        <p >Posting Time: 1/2/1999</p>
+                    </div>
+                </Card>
+                <Card style={{ width: 400, height: 250, cursor: "pointer", border: "1px solid" }}>
+                    <div className="posting-title">
+                        <img
+                            src="http://localhost:9000/images/companylogos/Plexxis-Logo-1695747169796.png"
+                            width={60}
+                            height={60}
+                            alt="" />
+                        <h2>Software Engineer</h2>
+                    </div>
+                    <div className="posting-detail">
+                        <div className="location">
+                            <p><CiLocationOn /></p>
+                            <p>Ontario</p>
+                            <p style={{ margin: 0, marginLeft: "10px" }}>100,000 cad</p>
+                        </div>
+                        <p >Posting Time: 1/2/1999</p>
+                    </div>
+                </Card>
+            </div>
+            <div className="pagination">
+                <Pagination defaultCurrent={1} total={50} />;
+            </div>
         </div>
     )
 }
