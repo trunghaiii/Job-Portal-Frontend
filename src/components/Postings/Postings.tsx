25861f73043bbef5790ea219e33641b404d96053
trunghaiii <haitran022001@gmail.com>
2023-09-29 00:03:02 -0400

    complete job postings searching feature

--- src/components/Postings/Postings.tsx
+++ src/components/Postings/Postings.tsx
@@ -1,4 +1,4 @@
-import { Card, Pagination } from "antd";
+import { Button, Card, Form, Input, Pagination } from "antd";
 import { CiLocationOn } from 'react-icons/ci';
 import "./Postings.scss"
 import { useEffect, useState } from "react";
@@ -6,7 +6,13 @@ import { getSearchJobsPagination } from "../../services/api";
 import moment from "moment";
 
 
+type FieldType = {
+    searchString?: string;
+};
+
 const Postings = () => {
+
+    const [form] = Form.useForm();
 
     const [jobData, setJobData] = useState<any>([])
 
@@ -14,16 +20,36 @@ const Postings = () => {
     const [limit, setLimit] = useState<number>(3)
     const [total, setTotal] = useState<number>(0)
 
+    const [queryString, setQueryString] = useState<string>("")
+
     const handlePagination = (page: number, pageSize: number) => {
         setCurrent(page)
 
+    }
+
+    const handleSearch = (values: any) => {
+        if (values.searchString === undefined) {
+            setQueryString("")
+        } else {
+            setQueryString(values.searchString)
+        }
+
+        setCurrent(1)
+    };
+
+    const handleReload = () => {
+        setQueryString("")
+        setCurrent(1)
+        form.resetFields()
     }
 
     const fetchAllJobs = async () => {
 
         // 1. call api
 
-        let response = await getSearchJobsPagination(`current=${current}&limit=${limit}&populate=company`)
+        let response = await getSearchJobsPagination(
+            `current=${current}&limit=${limit}&name=/${queryString}/i&populate=company`
+        )
         if (response && response.statusCode === 200) {
             setTotal(response.data.meta.total)
             setJobData(response.data.result)
@@ -32,11 +58,44 @@ const Postings = () => {
     }
     useEffect(() => {
         fetchAllJobs()
-    }, [current])
+    }, [current, queryString])
 
 
     return (
         <div className="posting-container">
+            <div className="posting-searching">
+                <Form
+                    form={form}
+                    name="basic"
+                    labelCol={{ span: 8 }}
+                    wrapperCol={{ span: 24 }}
+                    onFinish={handleSearch}
+                    autoComplete="off"
+                >
+                    <div style={{ display: "flex", alignItems: "center", justifyContent: "space-around" }}>
+                        <Form.Item<FieldType>
+                            //label="Company Name"
+                            name="searchString"
+                            style={{ width: "60%" }}
+                        >
+                            <Input placeholder="Searching Name" />
+                        </Form.Item>
+
+                        <Form.Item >
+                            <Button type="primary" htmlType="submit">
+                                Search
+                            </Button>
+                            <Button
+                                style={{ marginLeft: "5px" }}
+                                htmlType="submit"
+                                onClick={() => handleReload()}
+                            >
+                                Reload
+                            </Button>
+                        </Form.Item>
+                    </div>
+                </Form>
+            </div>
             <div className="posting-card">
                 {
                     jobData && jobData.length > 0
@@ -46,7 +105,7 @@ const Postings = () => {
                             <Card style={{ width: 400, height: 250, cursor: "pointer", border: "1px solid" }}>
                                 <div className="posting-title">
                                     <img
-                                        src={`http://localhost:9000/images/companylogos/${job.company.logo}`}
+                                        src={`${import.meta.env.VITE_BACKEND_URL}/images/companylogos/${job.company.logo}`}
                                         width={60}
                                         height={60}
                                         alt="" />
