eccdb0bd90e113522d3cc7b63252b4726ceb4401
trunghaiii <haitran022001@gmail.com>
2023-10-12 18:25:39 -0400

    design CompanyDetailModal component

--- src/components/HomeLayout/Home/CompanyDetail/CompanyDetailModal.tsx
+++ src/components/HomeLayout/Home/CompanyDetail/CompanyDetailModal.tsx
@@ -1,5 +1,5 @@
 import { Modal } from "antd";
-
+import "./CompanyDetailModal.scss"
 
 interface IProps {
     openCompanyDetailModal: boolean
@@ -19,13 +19,25 @@ const CompanyDetailModal = (props: IProps) => {
     };
     return (
         <Modal
-            title="Basic Modal"
+            title="Company Information"
             open={openCompanyDetailModal}
             onOk={handleOk}
             onCancel={handleCancel}>
-            <p>Some contents...</p>
-            <p>Some contents...</p>
-            <p>Some contents...</p>
+            <div className="company-detail-container">
+                <div className="title-group">
+                    <img
+                        src="http://localhost:9000/images/companylogos/Plexxis-Logo-1695956294169.png"
+                        alt="company-logo"
+                        height={70}
+                        width={70}
+                    />
+                    <h3>Plexxis Software</h3>
+                </div>
+                <div className="detail-group">
+                    <h4>(14 Abacus Rd, Brampton, ON L6T 5B)</h4>
+                    <p>Plexxis serves subcontractors who seek elite team cohesion and performance. Coupling cloud construction management software, on-premise and hosted solutions, we unite operations, estima</p>
+                </div>
+            </div>
         </Modal>
     )
 }
