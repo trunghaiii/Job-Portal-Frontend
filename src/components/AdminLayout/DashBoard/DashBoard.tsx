8deedebd7797060a1f098b5c9f91089cb396e7b8
trunghaiii <haitran022001@gmail.com>
2023-10-14 12:51:02 -0400

    fast-designing dash-board using antd

--- src/components/AdminLayout/DashBoard/DashBoard.tsx
+++ src/components/AdminLayout/DashBoard/DashBoard.tsx
@@ -1,8 +1,21 @@
 
+import CountUp from 'react-countup';
+import { Col, Row, Statistic } from 'antd';
+
+const formatter = (value: number) => <CountUp end={value} separator="," />;
 
 const DashBoard = () => {
     return (
-        <div>dash board</div>
+        <div>
+            <Row gutter={16}>
+                <Col span={12}>
+                    <Statistic title="Job Positions" value={112893} formatter={formatter} />
+                </Col>
+                <Col span={12}>
+                    <Statistic title="Total Users" value={112893} precision={2} formatter={formatter} />
+                </Col>
+            </Row>
+        </div>
     )
 }
 
